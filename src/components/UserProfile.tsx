9dfd647dbcfe8d5c6dd17a0f33e95732f2834993
pedro felippe <pedfelippe@gmail.com>
2024-01-19 22:56:05 -0300

    design: responsiveness

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -6,7 +6,7 @@ import { FaMapLocation } from "react-icons/fa6";
 const UserInfo = ({ info, title }: { info: number; title: string }) => {
   return (
     <div className="flex flex-col items-center  ">
-      <p className="text-3xl font-medium">{info}</p>
+      <p className="text-xl lg:text-3xl font-medium">{info}</p>
       <div className="text-lg">{title}</div>
     </div>
   );
@@ -17,17 +17,20 @@ function UserProfile() {
   if (!userData) return null;
 
   return (
-    <div className="flex justify-start text-lg  bg-gray-50 rounded-3xl w-fit mx-auto overflow-hidden ">
+    <div
+      className="flex max-lg:flex-col justify-start text-lg max-lg:max-w-md w-full
+     bg-gray-50 rounded-3xl lg:w-fit mx-auto overflow-hidden "
+    >
       <img
         src={userData?.avatar_url}
-        className="flex-shrink-1 object-cover  max-w-sm  "
+        className="flex-shrink-1 object-cover w-full lg:max-w-sm max-lg:max-h-[250px] "
       />
 
-      <div className="flex flex-col items-start justify-between  gap-6 px-12 p-8 w-full">
+      <div className="flex flex-col items-start justify-between  gap-6 p-6 lg:p-10 w-full">
         <div className="flex flex-col gap-3 w-full ">
           <CalculateStars />
           <span>
-            <div className="flex  gap-2 items-center text-3xl font-bold">
+            <div className="flex gap-2 items-center text-xl lg:text-3xl font-bold">
               {userData?.name}
               <h1 className="text-xl font-thin">@{userData?.login}</h1>
             </div>
@@ -48,7 +51,7 @@ function UserProfile() {
           )}
         </div>
 
-        <div className="flex items-start justify-start gap-12 w-full">
+        <div className="flex items-start justify-between w-full">
           <UserInfo info={userData?.followers} title="Followers" />
           <UserInfo info={userData?.following} title="Following" />
           <UserInfo info={userData?.public_repos} title="Repositories" />
--- src/pages/SearchGithubUsers.tsx
+++ src/pages/SearchGithubUsers.tsx
@@ -3,7 +3,7 @@ import UserProfile from "../components/UserProfile";
 
 function SearchGithubUsers() {
   return (
-    <div className="min-h-screen bg-gray-950  p-20 flex items-center">
+    <div className="min-h-screen bg-gray-950 py-16 px-8 lg:px-20 flex items-center">
       <div className=" w-full mx-auto items-center flex flex-col gap-8 h-full  justify-center">
         <p className="font-bold text-4xl text-slate-50 tracking-tighter">
           Search Github Users
