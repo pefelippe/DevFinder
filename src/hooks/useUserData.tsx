a88821c52bf251b86782acfe4d67d1af335168eb
pedro felippe <pedfelippe@gmail.com>
2024-01-22 15:18:16 -0300

    fix typo

--- src/hooks/useUserData.tsx
+++ src/hooks/useUserData.tsx
@@ -3,6 +3,8 @@ import { useContext } from "react";
 
 export const useUserData = () => {
   const context = useContext(UserContext);
-  if (!context) throw new Error("must be sed within a UserProvider");
+
+  if (!context) throw new Error("must be used within a UserProvider");
+
   return context;
 };
